141693e869d8d7b998bc6a38a8c55a451b6e69cf
Sasi <kumr.dav@gmail.com>
2026-04-20 14:55:38 -0400

    feat: INFRA-001 - remote state backend, folder structure, EC2 module scaffold

---
+++ bootstrap/backend-setup.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "nexabuild-terraform-state-kumar"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state_public_access_block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "nexabuild-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
